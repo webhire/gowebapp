7835734eb08ab3256cf00edc4680ac51822c63a1
Erkinov <erkinovit@gmail.com>
2023-01-11 18:53:04 +0500

    error cheking

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
 
-const portNumber = ":8082"
+const portNumber = ":8084"
 
 // Home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -23,9 +24,29 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100, 10)
+	if err != nil {
+		fmt.Fprintf(w, "cannot divide by 0")
+		return
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100, 10, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
